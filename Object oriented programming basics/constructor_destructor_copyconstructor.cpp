07620a5f544a5a9649cdd6433f5f13e7db9ef939
Eshika Shah <63183513+EshikaShah@users.noreply.github.com>
2020-11-29 13:11:48 +0530

    Add files via upload

---
+++ Object oriented programming basics/constructor_destructor_copyconstructor.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+static int count=0;
+
+class computer{
+    public:
+    string type,RAM,HDD,speed;
+    int price;
+    computer(string type1, string RAM1, string HDD1, string speed1, int price1)
+    {
+        type=type1;
+        RAM=RAM1;
+        HDD=HDD1;
+        speed=speed1;
+        price=price1;
+        count++;
+    }
+    computer(const computer &c2)
+    {
+        type=c2.type;
+        RAM=c2.RAM;
+        HDD=c2.HDD;
+        speed=c2.speed;
+        price=c2.price;
+        count++;
+    }
+    ~computer()
+    {
+        count--;
+        cout<<"Count is: "<<count<<endl;
+    }
+};
+int main()
+{
+    computer c1("Branded","2GB","1TB","10GHz",50000);
+    computer c2 = c1;
+
+    cout<<"Type: "<<c1.type<<" RAM: "<<c1.RAM<<" HDD: "<<c1.HDD<<" Speed: "<<c1.speed<<" Price: "<<c1.price<<endl;
+    cout<<"Type: "<<c2.type<<" RAM: "<<c2.RAM<<" HDD: "<<c2.HDD<<" Speed: "<<c2.speed<<" Price: "<<c2.price<<endl;
+}
