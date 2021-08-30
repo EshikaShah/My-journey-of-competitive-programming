3107ab8c44226d502e9672a3f3eb32dc171d1af1
Eshika Shah <63183513+EshikaShah@users.noreply.github.com>
2021-08-30 11:39:47 +0530

    Create rotate.py

---
+++ Python/Strings/rotate.py
@@ -0,0 +1,18 @@
+'''
+796. Rotate String
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+'''
+def rotateString(s, goal):
+    if(goal==s):
+        return True
+    a=len(s)
+    while(goal!=s and a>0):
+        goal=goal[1:]+goal[0]
+        a-=1
+        if(goal==s):
+            return True
+    return False
