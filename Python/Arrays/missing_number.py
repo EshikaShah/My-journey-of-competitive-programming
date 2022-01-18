8b5a6381454955036540cf6977521f064a39a346
Eshika Shah <63183513+EshikaShah@users.noreply.github.com>
2022-01-18 11:27:43 +0530

    Create missing_number.py

---
+++ Python/Arrays/missing_number.py
@@ -0,0 +1,9 @@
+'''
+Missing Number
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+'''
+
+def missingNumber(nums: List[int]) -> int:
+    n = len(nums)
+    return n*(n+1)//2 - sum(nums)
